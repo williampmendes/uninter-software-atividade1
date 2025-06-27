320a37877a1286f79ed5248106c1c5bfb1a69235
williampmendes <30899373+williampmendes@users.noreply.github.com>
2025-06-26 23:04:34 -0300

    Add files via upload

---
+++ scriptDev.js
@@ -0,0 +1,5 @@
+let botao = document.querySelector('#botao');
+let divTextoBotao = document.querySelector('#textoBotao');
+
+
+botao.addEventListener("click",e => {divTextoBotao.textContent = 'Obrigado por visitar o site!!!';});
